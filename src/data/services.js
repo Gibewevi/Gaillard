c250d351d7a6d0c77e15300eb7237d94c4c32cfa
gibewevi <pixelcoin1990@gmail.com>
2026-04-19 19:37:22 -0400

    feat: download and localize service images

--- src/data/services.js
+++ src/data/services.js
@@ -7,7 +7,7 @@ export const services = [
     description: "Indispensables pour les professions libérales. Un premier regard capital qui reflète votre image de marque.", 
     icon: ShieldCheck,
     details: "La plaque professionnelle est un outil incontournable pour avoir une bonne visibilité dans la rue. Elle doit être choisie avec soin car elle reflète votre image. Nous vous proposons des produits solides, de qualité et esthétiques (finition particulièrement soignée). Nos supports incluent le Plexiglass, l'Aluminium et le Laiton pour un rendu élégant et durable.",
-    image: "http://www.gaillard-gravure.fr/wp-content/uploads/Pro-A-la-une.jpg"
+    image: "../assets/services/plaques-professionnelles.jpg"
   },
   { 
     slug: "gravure-bijoux",
@@ -15,7 +15,7 @@ export const services = [
     description: "Précision artisanale sur métaux précieux.", 
     icon: Gem,
     details: "Doté d’un savoir faire unique, héritier d’une grande tradition, nous vous proposons une gravure à la main sur bijoux, argenterie, ainsi que tous métaux précieux. De la gravure en taille douce, ou à l’eau forte, découvrez des styles de gravures, textes ou dessins, personnalisés et à votre goût.",
-    image: "http://www.gaillard-gravure.fr/wp-content/uploads/GravMain-A-la-une.jpg"
+    image: "../assets/services/gravure-bijoux.jpg"
   },
   { 
     slug: "signaletique",
@@ -23,7 +23,7 @@ export const services = [
     description: "Solutions personnalisées pour vos locaux.", 
     icon: MapPin,
     details: "Signalétiques intembreures ou extérieures pour les particuliers ou les professionnels (copropriétés, parkings, syndic). Pictogrammes, panneaux et étiquettes d’indication, plaques de boîte aux lettres, totems… un large choix de produits pour bien signaler, orienter, et informer. Nous proposons de nombreuses matières et un large choix de couleurs.",
-    image: "http://www.gaillard-gravure.fr/wp-content/uploads/Signaletique-A-la-une.jpg"
+    image: "../assets/services/signaletique.jpg"
   },
   { 
     slug: "personnalisation",
@@ -31,7 +31,7 @@ export const services = [
     description: "Rendez vos objets uniques avec notre gravure.", 
     icon: Gift,
     details: "Parce que la gravure est éternelle et inimitable, nous personnalisons de nombreux objets. Smartphones, tablettes, ordinateurs, disques durs… Rendez les encore plus uniques ! Un grand nombre de supports disponibles : bois, cuir, ardoise, caoutchouc, plastique, aluminium, acier…",
-    image: "http://www.gaillard-gravure.fr/wp-content/uploads/Personnalisation-A-la-une.jpg"
+    image: "../assets/services/personnalisation.jpg"
   },
   { 
     slug: "evenements",
@@ -39,7 +39,7 @@ export const services = [
     description: "Gravure sur supports événementiels.", 
     icon: Calendar,
     details: "Inauguration, Commém	ations, Départs... Nous réalisons pour les entreprises, associations et particuliers des plaques pour tous les évènements importants. Nous gravons également les objets qui vous permettront d’immortaliser ces moments.",
-    image: "http://www.gaillard-gravure.fr/wp-content/uploads/Evenements-a-la-une.jpg"
+    image: "../assets/services/evenements.jpg"
   },
   { 
     slug: "gravure-industrielle",
@@ -47,7 +47,7 @@ export const services = [
     description: "Séries et gravures de précision industrielle.", 
     icon: Settings,
     details: "De la pièce unique à la petite série. Découpe et gravure laser. Marquage CE, marquage simple, caractéristiques techniques du matériel, traçabilité du produit, coordonnées de votre société sur le matériel, plaques machine, étiquettes de sécurité…",
-    image: "http://www.gaillard-gravure.fr/wp-content/uploads/Indus-a-la-une.jpg"
+    image: "../assets/services/gravure-industrielle.jpg"
   },
   { 
     slug: "timbre-a-sec",
@@ -55,7 +55,7 @@ export const services = [
     description: "Authentifiez et officialisez vos documents.", 
     icon: Layers,
     details: "Authentifiez, individualisez, ou officialisez vos documents ou vos photos, rendez les uniques et infalsifiables ! Les timbres à sec n’utilisent pas d’encre, mais gaufrent le papier pour exécuter une empreinte en relief, appelée gaufrage. Nous mettons également à votre disposition des cachets cires.",
-    image: "http://www.gaillard-gravure.fr/wp-content/uploads/Timbre-a-la-une.jpg"
+    image: "../assets/services/timbre-a-sec.jpg"
   },
   { 
     slug: "tampons",
@@ -63,6 +63,6 @@ export const services = [
     description: "Solutions professionnelles de marquage.", 
     icon: Award,
     details: "Tampons encreurs et solutions de marquage professionnel pour vos besoins administratifs et d'entreprise.",
-    image: "http://www.gaillard-gravure.fr/wp-content/uploads/2012/09/tampon-gaillardgravure.fr_.jpg"
+    image: "../assets/services/tampons.jpg"
   }
 ];
